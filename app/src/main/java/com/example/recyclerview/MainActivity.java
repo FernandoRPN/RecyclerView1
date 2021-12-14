5b0d14ab166afadab59102144d30c042a863382a
FernandoRPN <62890351+FernandoRPN@users.noreply.github.com>
2021-12-13 21:20:14 -0600

    Agregar nombres a Lista getallnames

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
             add("Manuel");
             add("Antonio");
             add("Roberto");
+            add("Nestor");
 
         }};
     }
